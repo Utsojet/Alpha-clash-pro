25d1fb2b8169ebb185c1019071784ee1bb8170a2
Utsojet <utsojetpaticor.cse9.bu@gmail.com>
2024-06-03 22:58:24 +0600

    added js

---
+++ scripts/home.js
@@ -0,0 +1,13 @@
+function play(){
+    // const homeSection = document.getElementById('home-screen');
+    // homeSection.classList.add('hidden');
+    // // show the playground
+    // const playgroundSection = document.getElementById('play-ground');
+    // playgroundSection.classList.remove('hidden');
+    hideElementById('home-screen');
+    showElementById('play-ground');
+}
+function continueGame(){
+    const alphabet = getARandomAlphabet();
+    
+}
